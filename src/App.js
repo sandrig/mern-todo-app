3b920cdce8f75d9f97b41c0b18d1642fb43d25b1
Andrey Savelyev <sandrig.dev@gmail.com>
2019-07-07 11:59:30 +0300

    feat: add react router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="container">
-        <h2>MERN-Stack Todo App</h2>
-      </div>
+      <Router>
+        <div className="container">
+          <h2>MERN-Stack Todo App</h2>
+        </div>
+      </Router>
     );
   }
 }
