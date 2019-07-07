e4afc1107074c824022546c16b7038804a915e2b
Andrey Savelyev <sandrig.dev@gmail.com>
2019-07-07 12:07:56 +0300

    refactor: replaced statefull component to stateless

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+export const App = () => (
+  <Router>
+    <div className="container">
+      <h2>MERN-Stack Todo App</h2>
+    </div>
+  </Router>
+);
