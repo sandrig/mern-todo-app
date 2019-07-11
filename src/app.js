5acc8ad8487368d7824dbfdff1f01fed3f135a40
Andrey Savelyev <sandrig.dev@gmail.com>
2019-07-11 21:39:46 +0300

    feat: add base markup for App component

--- src/app.js
+++ src/app.js
@@ -1,11 +1,37 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import { CreateTodo } from "./components/create-todo";
+import { EditTodo } from "./components/edit-todo";
+import { TodosList } from "./components/todos-list";
 
 export const App = () => (
   <Router>
     <div className="container">
-      <h2>MERN-Stack Todo App</h2>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <Link to="/" className="navbar-brand">
+          MERN-Stack Todo App
+        </Link>
+        <div className="collpase navbar-collapse">
+          <ul className="navbar-nav mr-auto">
+            <li className="navbar-item">
+              <Link to="/" className="nav-link">
+                Todos
+              </Link>
+            </li>
+            <li className="navbar-item">
+              <Link to="/create" className="nav-link">
+                Create Todo
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <br />
+      <Route path="/" exact component={TodosList} />
+      <Route path="/edit/:id" component={EditTodo} />
+      <Route path="/create" component={CreateTodo} />
     </div>
   </Router>
 );
