5cc0713f37b107f4341c1fcbcba182ee1f6da000
Andrey Savelyev <sandrig.dev@gmail.com>
2019-07-16 23:00:37 +0300

    feat: add completed className

--- src/components/todo.js
+++ src/components/todo.js
@@ -3,9 +3,9 @@ import { Link } from 'react-router-dom';
 
 export const Todo = ({ todo }) => (
   <tr>
-    <td>{todo.description}</td>
-    <td>{todo.responsible}</td>
-    <td>{todo.priority}</td>
+    <td className={todo.completed ? 'completed' : ''}>{todo.description}</td>
+    <td className={todo.completed ? 'completed' : ''}>{todo.responsible}</td>
+    <td className={todo.completed ? 'completed' : ''}>{todo.priority}</td>
     <td>
       <Link to={`/edit/${todo._id}`}>Edit</Link>
     </td>
